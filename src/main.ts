75a7c9a6e5c1d2485f77f789f0ce81ef031bc8f5
Muhammad Usama Shahid <muhammadusama387@gmail.com>
2024-04-16 12:39:29 +0500

    Initial project setup

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+
+import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule), provideAnimations()],
+}).catch((err) => console.error(err));
+
