664ccbe2b05d4ee4664ddbf292c818e5d75377a1
Muhammad Usama Shahid <muhammadusama387@gmail.com>
2024-04-16 19:44:11 +0500

    chore: added env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+const BASE_DOMAIN = `https://bikeportal.cyclic.app`;
 export const environment = {
     production: true,
-};
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzYW1hIiwiaWF0IjoxNzEzMjA4MjUxLCJleHAiOjE3MTMyMTAwNTF9.z062_VB04hteNabnSKha6n2i4CEe5zjiKSJrPu9-UWA',
+
+    USER_BASE_URL: '/api/user',
+    BIKE_BASE_URL: BASE_DOMAIN + '/bikes',
+    BIKE: {
+        GET_BY_HANDLE: '/by-handle',
+        SEARCH_BY_TITLE: '/search/by-title',
+    }
+  };
