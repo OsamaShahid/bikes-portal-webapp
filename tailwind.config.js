bf8367cc20b9d9921111ce1d34fc7b053afb7630
Muhammad Usama Shahid <muhammadusama387@gmail.com>
2024-04-16 12:40:18 +0500

    Initial project setup (#1)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// const colors = require('tailwindcss/colors');
+
+module.exports = {
+  prefix: '',
+  mode: 'jit',
+  important: false,
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+  },
+  variants: {
+  },
+  plugins: [
+  ],
+};
